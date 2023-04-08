7195a91a1e0c0e18b95d59eb2f810175afa7df3a
Амаль <50665935+WTTeneger@users.noreply.github.com>
2023-04-08 18:35:18 +0700

    rss

--- express/server.js
+++ express/server.js
@@ -4,6 +4,27 @@ const path = require('path');
 const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
+const fetch = require('node-fetch');
+
+
+async function getNewsArea(account = "@amal_agishev", post_id = 'XCi4p26c0Tl') {
+    let BlogState = ''
+    var requestOptions = {
+        method: 'GET',
+        redirect: 'manual'
+    };
+
+    let a = await fetch(`https://teletype.in/${account}/${post_id}`, requestOptions)
+        .then(response => response.text())
+        .then(result => {
+            // получить <article>   </article>
+            let article = result.match(/<article(.*?)<\/article>/g)
+            // console.log(article);
+            return article
+        })
+        .catch(error => console.log('error', error));
+    return a
+}
 
 const router = express.Router();
 router.get('/', (req, res) => {
@@ -14,8 +35,19 @@ router.get('/', (req, res) => {
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
 
+router.get('/post', (req, res) => {
+    let account = req.query.account || "@amal_agishev"
+    let post_id = req.query.post_id || 'XCi4p26c0Tl'
+    let a = getNewsArea(account, post_id).then((data) => {
+        console.log('data', data);
+        if (!data) return res.json({ post: 'error' })
+        return res.json({ post: data[0] })
+    })
+    return a 
+})
+
 app.use(bodyParser.json());
-app.use('/.netlify/functions/server', router);  // path must route to lambda
+app.use('/server', router);  // path must route to lambda
 app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 
 module.exports = app;
